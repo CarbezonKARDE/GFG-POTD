8ec271856692085cd38b131ce240b9ccd40df278
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2024-08-31 15:46:11 +0530

    Create 31-08-24 gfg.cpp

---
+++ 31-08-24 gfg.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    vector<int> find3Numbers(vector<int> &arr) {
+     int n = arr.size();
+        vector<int> suffMax = arr;
+        for(int i = n-2; i >= 0; i--)
+            suffMax[i] = max(suffMax[i+1], arr[i]);
+        
+        int minStart = arr[0];
+        for(int i = 1; i < n-1; i++) {
+            int maxEnd = suffMax[i+1];
+            int curr = arr[i];
+            if(minStart < curr && curr < maxEnd) {
+                return {minStart, curr, maxEnd};
+            }
+            minStart = min(minStart, curr);
+        }
+        return {};  
+    }
+};
