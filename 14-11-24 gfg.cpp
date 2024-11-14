3b375c0f9a0988a9b7651a2f2a203c9de73a2783
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2024-11-14 09:04:33 +0530

    Create 14-11-24 gfg.cpp

---
+++ 14-11-24 gfg.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    void nearlySorted(vector<int>& arr, int k) {
+        priority_queue<int>pq;
+        int i=0,j=0;
+        while(j < arr.size()){
+            pq.push(-1*arr[j]);
+            if(pq.size() < k+1){
+                j++;
+            }
+            else{
+                arr[i] = -1*pq.top();
+                pq.pop();
+                i++;j++;
+            }
+        }
+        while(!pq.empty()){
+            arr[i++] = -1 * pq.top();
+            pq.pop();
+        }
+    }
+};
