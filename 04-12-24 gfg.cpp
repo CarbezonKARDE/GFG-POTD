150e72264a0bf7b4cba076966a1ff85a2ae79e38
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2024-12-04 17:53:43 +0530

    Create 04-12-24 gfg.cpp

---
+++ 04-12-24 gfg.cpp
@@ -0,0 +1,8 @@
+class Solution {
+  public:
+    bool areRotations(string &s1, string &s2) {
+        if(s1.size()!=s2.size()) return false;
+        s1=s1+s1;
+        return s1.find(s2)!=string::npos;
+    }
+};
