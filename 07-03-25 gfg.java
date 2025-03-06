b983f92fd1265246b93a6929afe4c54474ba8e87
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2025-03-07 01:56:50 +0530

    Create 07-03-25 gfg.java

---
+++ 07-03-25 gfg.java
@@ -0,0 +1,28 @@
+class Solution {
+    static int[][] dp;
+    public boolean isPallin(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        return sb.reverse().toString().equals(s);
+    }
+    public int check(String s, int i, int j) {
+        if (i > j) return 0;
+        if (i == j) return 1;
+        if (dp[i][j] != -1) return dp[i][j];
+        if (s.charAt(i) == s.charAt(j)) {
+            dp[i][j] = 2 + check(s, i + 1, j - 1);
+        } else {
+            dp[i][j] = Math.max(check(s, i + 1, j), check(s, i, j - 1));
+        }
+        return dp[i][j];
+    }
+    public int longestPalinSubseq(String s) {
+        int n = s.length();
+        dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        return check(s, 0, n - 1);
+    }
+}
