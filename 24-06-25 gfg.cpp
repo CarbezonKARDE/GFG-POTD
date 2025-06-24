500076a69a272dc750a07295953f9fee998d6042
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2025-06-25 03:03:55 +0530

    Create 24-06-25 gfg.cpp

---
+++ 24-06-25 gfg.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    string maxSubseq(string& s, int k) {
+        int n = s.size();
+        int keep = n - k;
+        string result;
+        for (char c : s) {
+            while (!result.empty() && k > 0 && result.back() < c) {
+                result.pop_back();
+                k--;
+            }
+            result.push_back(c);
+        }
+        if (result.size() > keep) {
+            result.resize(keep);
+        }
+        return result;
+    }
+};
