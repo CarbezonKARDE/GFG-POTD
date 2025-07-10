f22349a43aeae6e8251f0d6cf00f144dc4d80441
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2025-07-11 03:10:00 +0530

    Create 11-07-25 gfg.java

---
+++ 11-07-25 gfg.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countConsec(int n) {
+        int[] a = new int[n + 1];
+        int[] b = new int[n + 1];
+        a[1] = 1;
+        b[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            a[i] = a[i - 1] + b[i - 1];
+            b[i] = a[i - 1];
+        }
+        int withoutConsec = a[n] + b[n];
+        int total = (int) Math.pow(2, n);
+        return total - withoutConsec;
+    }
+}
