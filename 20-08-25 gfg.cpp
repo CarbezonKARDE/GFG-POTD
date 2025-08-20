a5acc2d9df56361bdc4917bccf1553f3f4fc3f44
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2025-08-20 16:29:11 +0530

    Create 20-08-25 gfg.cpp

---
+++ 20-08-25 gfg.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>> &mat, int x) {
+        int n = mat.size(), m = mat[0].size();
+        int left = 0, right = n * m - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int row = mid / m, col = mid % m;
+            int midVal = mat[row][col];
+            if (midVal == x) return true;
+            int leftVal = mat[left / m][left % m];
+            int rightVal = mat[right / m][right % m];
+            if (leftVal <= midVal) { 
+                if (leftVal <= x && x < midVal) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else { 
+                if (midVal < x && x <= rightVal) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+};
