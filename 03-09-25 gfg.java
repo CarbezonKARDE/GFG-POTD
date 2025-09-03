69830d61bd8918c2d91315d76fa23779be86afb7
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2025-09-03 17:48:30 +0530

    Create 03-09-25 gfg.java

---
+++ 03-09-25 gfg.java
@@ -0,0 +1,15 @@
+class Solution {
+    public Node reverse(Node head) {
+        if (head == null || head.next == null) return head;
+        Node curr = head;
+        Node newHead = null;
+        while (curr != null) {
+            Node temp = curr.prev;
+            curr.prev = curr.next;
+            curr.next = temp;
+            newHead = curr;
+            curr = curr.prev;
+        }
+        return newHead;
+    }
+}
