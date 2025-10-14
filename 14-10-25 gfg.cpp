2dccd474c30c03bbacf7815ddb3da31eaa53ab20
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2025-10-14 16:03:53 +0530

    Create 14-10-25 gfg.cpp

---
+++ 14-10-25 gfg.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int nodeSum(Node* root, int l, int r) {
+        if (root == NULL) return 0;
+        if (root->data < l) {
+            return nodeSum(root->right, l, r);
+        }
+        if (root->data > r) {
+            return nodeSum(root->left, l, r);
+        }
+        return root->data + nodeSum(root->left, l, r) + nodeSum(root->right, l, r);
+    }
+};
