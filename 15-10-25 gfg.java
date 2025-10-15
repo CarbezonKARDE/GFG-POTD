d9e30ce6c94c57dca9f439a6a9e48716ed7d4cc9
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2025-10-15 16:26:39 +0530

    Create 15-10-25 gfg.java

---
+++ 15-10-25 gfg.java
@@ -0,0 +1,18 @@
+class Solution {
+    int count = 0;
+    int result = -1;
+    public int kthSmallest(Node root, int k) {
+        inorder(root, k);
+        return result;
+    }
+    private void inorder(Node root, int k) {
+        if (root == null) return;
+        inorder(root.left, k);
+        count++;
+        if (count == k) {
+            result = root.data;
+            return;
+        }
+        inorder(root.right, k);
+    }
+}
