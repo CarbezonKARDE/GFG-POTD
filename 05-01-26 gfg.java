ca05efe4feb6ae97217e57f5043908fca9345a88
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2026-01-05 13:07:31 +0530

    Create 05-01-26 gfg.java

---
+++ 05-01-26 gfg.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubarraySum(int[] arr, int k) {
+        int n = arr.length;
+        int windowSum = 0;
+        for (int i = 0; i < k; i++) {
+            windowSum += arr[i];
+        }
+        int maxSum = windowSum;
+        for (int i = k; i < n; i++) {
+            windowSum += arr[i];
+            windowSum -= arr[i - k];
+            maxSum = Math.max(maxSum, windowSum);
+        }
+        return maxSum;
+    }
+}
