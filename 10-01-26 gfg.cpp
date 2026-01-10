9c8967d77b1d0ed1215084a542fd7da36e2d3e13
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2026-01-10 17:39:17 +0530

    Create 10-01-26 gfg.cpp

---
+++ 10-01-26 gfg.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long atMostK(string &s, int k) {
+        if (k < 0) return 0;
+        vector<int> freq(26, 0);
+        long long count = 0;
+        int left = 0;
+        for (int right = 0; right < s.size(); right++) {
+            if (freq[s[right] - 'a'] == 0)
+                k--;
+            freq[s[right] - 'a']++;
+            while (k < 0) {
+                freq[s[left] - 'a']--;
+                if (freq[s[left] - 'a'] == 0)
+                    k++;
+                left++;
+            }
+            count += (right - left + 1);
+        }
+        return count;
+    }
+    long long countSubstr(string& s, int k) {
+        return atMostK(s, k) - atMostK(s, k - 1);
+    }
+};
