c98287cf29afe3580de3046afba58f2054116e11
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2026-02-09 13:42:45 +0530

    Create 09-02-26 gfg.java

---
+++ 09-02-26 gfg.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findKRotation(int arr[]) {
+        int n = arr.length;
+        int low = 0, high = n - 1;
+        if (arr[low] <= arr[high]) {
+            return 0;
+        }
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (mid > 0 && arr[mid] < arr[mid - 1]) {
+                return mid;
+            }
+            if (mid < n - 1 && arr[mid] > arr[mid + 1]) {
+                return mid + 1;
+            }
+            if (arr[mid] >= arr[low]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return 0;
+    }
+}
