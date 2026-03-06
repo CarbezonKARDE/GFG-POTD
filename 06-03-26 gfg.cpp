a8161ea12b1e430fb285562744a88ba10126f312
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2026-03-06 14:39:29 +0530

    Create 06-03-26 gfg.cpp

---
+++ 06-03-26 gfg.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    string minWindow(string &s, string &p) {
+        vector<int> freq(26, 0);
+        for(char c : p) freq[c - 'a']++;
+        int left = 0, count = 0;
+        int minLen = INT_MAX, start = 0;
+        for(int right = 0; right < s.size(); right++) {
+            if(freq[s[right] - 'a'] > 0)
+                count++;
+            freq[s[right] - 'a']--;
+            while(count == p.size()) {
+                if(right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+                freq[s[left] - 'a']++;
+                if(freq[s[left] - 'a'] > 0)
+                    count--;
+                left++;
+            }
+        }
+        if(minLen == INT_MAX) return "";
+        return s.substr(start, minLen);
+    }
+};
