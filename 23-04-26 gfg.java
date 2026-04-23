8b080ed1aee14b37424f4acc9b23e9dd696c9fbc
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2026-04-23 10:53:16 +0530

    Create 23-04-26 gfg.java

---
+++ 23-04-26 gfg.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canSplit(int arr[]) {
+        int total = 0;
+        for (int num : arr) {
+            total += num;
+        }
+        if (total % 2 != 0) return false;
+        int prefixSum = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            prefixSum += arr[i];
+            if (prefixSum == total / 2) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
