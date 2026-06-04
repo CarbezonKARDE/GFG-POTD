62a35bd41be583b13019b5d42c2a804a56b6a323
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2026-06-04 12:44:29 +0530

    Create 04-06-26 gfg.cpp

---
+++ 04-06-26 gfg.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int maxSubstring(string &s) {
+        int curr = 0, maxi = -1;
+        for(char c : s) {
+            int val = (c == '0') ? 1 : -1;
+            curr = max(val, curr + val);
+            maxi = max(maxi, curr);
+        }
+        return maxi;
+    }
+};
