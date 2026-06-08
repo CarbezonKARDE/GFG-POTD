34b6a91601be200def6fb2ee34c6cea6baeb5f53
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2026-06-08 16:10:07 +0530

    Create 08-06-26 gfg.cpp

---
+++ 08-06-26 gfg.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    Node* reverseList(Node* head) {
+        Node* prev = nullptr;
+        Node* curr = head;
+        while (curr) {
+            Node* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+    Node *compute(Node *head) {
+        if (!head || !head->next) return head;
+        head = reverseList(head);
+        int mx = head->data;
+        Node* curr = head;
+        while (curr && curr->next) {
+            if (curr->next->data < mx) {
+                Node* temp = curr->next;
+                curr->next = temp->next;
+                delete temp;
+            } else {
+                curr = curr->next;
+                mx = curr->data;
+            }
+        }
+        return reverseList(head);
+    }
+};
