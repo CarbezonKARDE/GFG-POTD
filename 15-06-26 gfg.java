da0c38ba684dcbce84cdf4332790b7826028fa05
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2026-06-15 10:55:00 +0530

    Create 15-06-26 gfg.java

---
+++ 15-06-26 gfg.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minimumCost(int[] cost, int w) {
+        int INF = Integer.MAX_VALUE / 2;
+        int[] dp = new int[w + 1];
+        Arrays.fill(dp, INF);
+        dp[0] = 0;
+        for (int i = 0; i < cost.length; i++) {
+            if (cost[i] == -1) continue;
+            int wt = i + 1;
+            for (int j = wt; j <= w; j++) {
+                dp[j] = Math.min(dp[j], dp[j - wt] + cost[i]);
+            }
+        }
+        return dp[w] >= INF ? -1 : dp[w];
+    }
+}
