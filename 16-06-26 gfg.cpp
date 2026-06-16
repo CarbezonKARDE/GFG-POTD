1c0e397876fe5813416b1faacb71dd1eedab65a8
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2026-06-16 13:12:41 +0530

    Create 16-06-26 gfg.cpp

---
+++ 16-06-26 gfg.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    vector<int> constructList(vector<vector<int>> &queries) {
+        vector<int> arr;
+        int xr = 0;
+        arr.push_back(0);
+        for (auto &q : queries) {
+            int type = q[0];
+            int x = q[1];
+            if (type == 0) {
+                arr.push_back(x ^ xr);
+            } else {
+                xr ^= x;
+            }
+        }
+        for (int &val : arr) {
+            val ^= xr;
+        }
+        sort(arr.begin(), arr.end());
+        return arr;
+    }
+};
