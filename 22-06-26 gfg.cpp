eed0b55e5244dfcc0557a5cc69cc5d568ca5cc4f
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2026-06-22 15:18:14 +0530

    Create 22-06-26 gfg.cpp

---
+++ 22-06-26 gfg.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxArea(vector<int> &height) {
+        int left = 0, right = height.size() - 1;
+        int ans = 0;
+        while (left < right) {
+            int width = right - left - 1;
+            int area = min(height[left], height[right]) * width;
+            ans = max(ans, area);
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+        return ans;
+    }
+};
