5c0dc380c4bf7ec25a11d7d982e9819a70895780
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2026-07-01 02:49:08 +0530

    Create 30-06-26 gfg.cpp

---
+++ 30-06-26 gfg.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minInsAndDel(vector<int> &a, vector<int> &b) {
+        unordered_map<int, int> pos;
+        for (int i = 0; i < b.size(); i++)
+            pos[b[i]] = i;
+        vector<int> lis;
+        for (int x : a) {
+            if (!pos.count(x)) continue;
+            int idx = pos[x];
+            auto it = lower_bound(lis.begin(), lis.end(), idx);
+            if (it == lis.end())
+                lis.push_back(idx);
+            else
+                *it = idx;
+        }
+        int L = lis.size();
+        return (a.size() - L) + (b.size() - L);
+    }
+};
