49dec742798d9ea9b98729b99589d2ba6dd3a48a
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2026-07-13 19:06:29 +0530

    Create 13-07-26 gfg.java

---
+++ 13-07-26 gfg.java
@@ -0,0 +1,46 @@
+class Solution {
+    static final int MOD = 1000000007;
+    int minOperations(int[] b) {
+        int n = b.length;
+        boolean[] vis = new boolean[n];
+        int[] spf = new int[n + 1];
+        for (int i = 0; i <= n; i++) spf[i] = i;
+        for (int i = 2; i * i <= n; i++) {
+            if (spf[i] == i) {
+                for (int j = i * i; j <= n; j += i) {
+                    if (spf[j] == j) spf[j] = i;
+                }
+            }
+        }
+        HashMap<Integer, Integer> maxExp = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                int len = 0;
+                int cur = i;
+                while (!vis[cur]) {
+                    vis[cur] = true;
+                    cur = b[cur] - 1;
+                    len++;
+                }
+                int x = len;
+                while (x > 1) {
+                    int p = spf[x];
+                    int cnt = 0;
+                    while (x % p == 0) {
+                        x /= p;
+                        cnt++;
+                    }
+                    maxExp.put(p, Math.max(maxExp.getOrDefault(p, 0), cnt));
+                }
+            }
+        }
+        long ans = 1;
+        for (int p : maxExp.keySet()) {
+            int e = maxExp.get(p);
+            for (int i = 0; i < e; i++) {
+                ans = (ans * p) % MOD;
+            }
+        }
+        return (int) ans;
+    }
+}
